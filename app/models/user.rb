cb58d2cea01645b8bdffdc18a7576c38915998e0
bodrovis <ibodrov@campaigner.ru>
2015-02-24 19:10:23 +0300

    Some fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,7 +25,10 @@ class User < ActiveRecord::Base
 
   class << self
     def current_users
-      sorcery_adapter.get_current_users
+      where("#{sorcery_config.last_activity_at_attribute_name} IS NOT NULL") \
+  .where("#{sorcery_config.last_logout_at_attribute_name} IS NULL
+    OR #{sorcery_config.last_activity_at_attribute_name} > #{sorcery_config.last_logout_at_attribute_name}") \
+  .where("#{sorcery_config.last_activity_at_attribute_name} > ? ", sorcery_config.activity_timeout.seconds.ago.utc.to_s(:db))
     end
   end
 end
